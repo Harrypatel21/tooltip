808af7cd210565c24ab637e704958ffd0916fef4
Keshav <keshav@full630-pro.local>
2023-07-30 23:05:11 +0530

    adding comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+
 import ToolTip from "./ToolTip";
 
 function App() {
   return (
     <div className="App">
+      {/* ToolText prop: Text to be displayed in the tooltip */}
+      {/* position prop: The position of the tooltip (top, bottom, right, left) */}
       <ToolTip ToolText="This click will delete all content" position="top">
-        <button className="button">Delete All </button>
+        {/* Button element inside the ToolTip component */}
+        <button className="button">Delete All</button>
       </ToolTip>
     </div>
   );
--- src/ToolTip.js
+++ src/ToolTip.js
@@ -2,10 +2,18 @@ import React from "react";
 
 export default function ToolTip(props) {
   return (
+    // Container for the tooltip trigger element
     <div className="tooltip-trigger">
+      {/* Tooltip element */}
+      {/* The tooltip's position is determined by the "position" prop */}
+      {/* The class "tooltip" is combined with "tooltip-{position}" to apply the correct position styling */}
       <div className={`tooltip tooltip-${props.position}`}>
+        {/* The text to be displayed inside the tooltip */}
         {props.ToolText}
       </div>
+
+      {/* The child elements passed to the ToolTip component */}
+      {/* These child elements are displayed along with the tooltip */}
       {props.children}
     </div>
   );
